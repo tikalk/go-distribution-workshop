6e10fae32d95b2857b477e6de51ca334391b6279
roy_pearl <royp@sentinelone.com>
2019-05-05 10:50:43 +0300

    Dependencies install

---
+++ dep.bat
@@ -0,0 +1,4 @@
+go get "github.com/matryer/vice/"
+go get "github.com/go-redis/redis"
+go get "github.com/urfave/cli"
+go get "github.com/mgutz/ansi"
---
+++ dep.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+go get "github.com/matryer/vice/"
+go get "github.com/go-redis/redis"
+go get "github.com/urfave/cli"
+go get "github.com/mgutz/ansi"
+
