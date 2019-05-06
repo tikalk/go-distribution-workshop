070e7a0e6a9e2433b57796665b87e3c1cae660ff
roy_pearl <royp@sentinelone.com>
2019-05-06 18:26:16 +0300

    cleanup

--- apps/game.go
+++ apps/game.go
@@ -19,9 +19,6 @@ func JoinGame(players []string, team models.Team, externalWaitGroup *sync.WaitGr
 	wg := &sync.WaitGroup{}
 	wg.Add(numPlayers)
 
-	// TODO Challenge: get a display input channel here
-
-
 	for i := 0; i < numPlayers; i++ {
 		u2 := uuid.NewV4()
 
@@ -39,7 +36,6 @@ func JoinGame(players []string, team models.Team, externalWaitGroup *sync.WaitGr
 		}
 		fmt.Printf("Added player %s\n", players[i])
 
-		// TODO Challenge: pass display input channel to the player
 		player.Activate(wg)
 
 	}
@@ -58,8 +54,6 @@ func ExecuteSimulation(numPlayers int, externalWaitGroup *sync.WaitGroup) {
 	rand.Seed(time.Now().UnixNano())
 	wg := &sync.WaitGroup{}
 	wg.Add(numPlayers)
-
-	// TODO Challenge: get a display input channel here
 
 	fmt.Println("Adding players...")
 
@@ -80,7 +74,6 @@ func ExecuteSimulation(numPlayers int, externalWaitGroup *sync.WaitGroup) {
 			player.TeamID = models.TeamBlue
 		}
 
-		// TODO Challenge: pass display input channel to the player
 		player.Activate(wg)
 	}
 
